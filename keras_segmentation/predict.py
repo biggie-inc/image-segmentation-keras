c160823e84eb0939ad8faced2898b073a317d509
Dan Cassin <dancassin@gmail.com>
2020-11-08 21:59:07 -0800

    print statements for window cntr only

--- keras_segmentation/predict.py
+++ keras_segmentation/predict.py
@@ -314,11 +314,14 @@ def predict(model=None, inp=None, out_fname=None,
     pr_resized = cv2.resize(pr_reshape, dsize=(inp.shape[1], inp.shape[0]), interpolation=cv2.INTER_NEAREST) #(960,1280,1)
     # np.savetxt('pr_resized.txt', pr_resized, delimiter=',', fmt='%i')
     pr_main_contours, window_cntr_only = largest_contours(pr_resized, n_classes) # returns numpy array with largest contour of each class
-    
-    xmin, xmax, ymin, ymax  = get_window_xy_min_max(window_cntr_only)
+    print(f'window cntr only shape: {(window_cntr_only.shape}')
+    print(f'window cntr only unique: {np.unique(window_cntr_only)}')
+    print(f'window cntr only: {window_cntr_only}')
+
+    wco_xmin, wco_xmax, wco_ymin, wco_ymax  = get_window_xy_min_max(window_cntr_only)
+    print(f'window cntr only min maxs: {wco_xmin}, {wco_xmax}, {wco_ymin}, {wco_ymax}')
     window_cntr_only = window_cntr_only.reshape(960, 1280, 1)
-    cv2.imwrite(f'./predictions/window_contour.jpg', window_cntr_only)
-    window_contour_cropped = get_cropped(window_cntr_only, [xmin, ymin, xmax, ymax])
+    window_contour_cropped = get_cropped(window_cntr_only, [wco_xmin, wco_ymin, wco_xmax, wco_ymax])
     #####
 
 
